5948623121f5b53f40b17304f1f66740dd2543da
ywfzvxc221 <sscccrr12@gmail.com>
2025-05-16 15:44:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "ضع_توكن_البوت_هنا"
+ADMIN_ID = 123456789  # معرف الأدمن (يمكن تعديله لاحقًا)
+
+PAYMENT_INFO = {
+    "faucetpay": "qqw112277331@gmail.com",
+    "binance": "TAwLiJmH7RBYf61MKXxSHZ9K81ysr1ZFkW"
+}
